8df6eb45cffa6ee37f2a9188ddbaf083c8b90bb1
Zaid Qureshi <zaidqureshi92@gmail.com>
2020-02-13 21:15:56 -0600

    commit 4

---
+++ Plot4.R
@@ -0,0 +1,14 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+library(stringr)
+filt1 <-  NEI %>% select(Emissions, year, SCC) 
+filt2 <- SCC %>% filter((str_detect(str_to_lower(SCC.Level.Four), "coal")) & (str_detect(str_to_lower(SCC.Level.One), "comb")))
+filt3 <- merge(filt1,filt2, by = "SCC") %>% select(Emissions, year) %>% group_by(year) %>%
+  summarise(T_Emissions = sum(Emissions)/10^5)
+filt3$year <- as.factor(filt3$year)
+
+png("plot4.png", width=480, height=480)
+with(filt3,barplot(T_Emissions,names.arg = year,xlab = "year", ylab = "total pm2.5 emissions(10^5 tons)", main ="emissions from coal combustion-related sources across US from 1999-2008"))
+dev.off()
+
+
